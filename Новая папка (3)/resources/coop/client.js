ac122044e98ecdb405c1d3c87f146392de300499
RetroMasterCode <krasavabrox@vk.com>
2025-06-03 08:08:04 +0300

    Delete Новая папка (3)/resources/coop directory

--- Новая папка (3)/resources/coop/client.js
+++
@@ -1,9 +0,0 @@
-addNetworkHandler("StartMission", function (missionId) {
-    gta.startMission(missionId);
-});
-addNetworkHandler("StartMission", function (bool) {
-    gta.cancelMission(bool);
-});
-addNetworkHandler("RestoreCamera", function (bool) {
-    gta.restoreCamera(bool);
-});
